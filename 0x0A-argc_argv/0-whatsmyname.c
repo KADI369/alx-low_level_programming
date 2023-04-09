a43e830e2d2d270d0b78f0c1ff997047871a3fcf
khadija <khadijabourabah15@gmail.com>
2023-04-09 15:00:16 -0700

    program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main -  a program that prints its name.
+ *@argc: count arguments
+ *@argv:array of arguments
+ *Return: Alwatys 0 (success).
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
